96bc1326aae8e269bb60e2c742b8b1bcb2b75071
dmunozpa <93115743+dmunozpa@users.noreply.github.com>
2025-09-11 19:06:12 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,4 +4,6 @@ SET USER_ID=$(id -u)
 SET GROUP_ID=$(id -g)
 :: SET API_KEY = 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
 :: docker login -u '$oauthtoken' -p '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' nvcr.io
+docker login -u '$oauthtoken' nvcr.io
 docker compose -f docker_compose.yaml up evaluator -d
+
